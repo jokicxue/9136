739da2af7645af6fbd27dd634a3338a512836568
Tiancheng XUE <jokicxue@gmail.com>
2024-07-31 13:13:40 +1000

    9136

--- assignments1/week3/task6.py
+++ assignments1/week3/task6.py
@@ -3,17 +3,15 @@ plist = ["12345","abcde","pass1","qwert","aaaaa","zzzzz","11111","apple","hello"
 def search_fun(a):
     i = len(alist)
     m = 0
-    n = 0
     test = str(input("Enter username:"))
     test1 = str(input(" Enter password:"))
     for m in range(i):
         if test == alist[m]:
-            for n in range(i):
-                if test1 == plist[n]:
-                    print(f" Login successful. Welcome {alist[m]} !")
-                    exit(0)
-                else:
-                    n += 1
+            if test1 == plist[m]:
+                print(f" Login successful. Welcome {alist[m]} !")
+                exit(0)
+            else:
+                break
         else:
             m += 1
     print(f" Login incorrect. Tries left: {a}")
@@ -24,3 +22,14 @@ x = 3
 while x > 0:
     x -= 1
     search_fun(x)
+    while x == 0:
+        qa = str(input("Are you a robot (Y/n)? "))
+        if qa == "n":
+            x = 3
+            break
+        elif qa == "Y":
+            exit(0)
+        elif qa == "":
+            exit(0)
+        else:
+            continue
--- assignments1/week3/test1.py
+++
@@ -1,5 +0,0 @@
-a=str(input("Enter a string: "))
-print(a)
-plist = ["12345","abcde","pass1","qwert","aaaaa","zzzzz","11111","apple","hello","admin"]
-if a == plist[0]:
-    print(f" Login successful. Welcome  !")
