d902439452b27b9cd906efd0ab0106913507dd9d
Tiancheng XUE <jokicxue@gmail.com>
2024-08-11 09:40:34 +1000

    9136

--- assignments1/week4/task7.py
+++ assignments1/week4/task7.py
@@ -5,43 +5,41 @@ def move(x0, y0, x1, y1):
     x = x1 - x0
     y = y1 - y0
 
-    if x < 0:
-        moves.append('l'*(-x))
-    elif x > 0:
-        moves.append('r'*x)
     if y < 0:
-        moves.append('u'*(-y))
+        moves.append('l'*(-y))
     elif y > 0:
-        moves.append('d'*y)
+        moves.append('r'*y)
+    if x < 0:
+        moves.append('u'*(-x))
+    elif x > 0:
+        moves.append('d'*x)
 
     moves.append('p')
-    return x1,y1
+    return x1, y1
 
-#主函数
-string = input("Enter a string to type:")
-moves = []
-if not string.isalpha():
-    print(f"The string cannot be typed out.")
-    exit(0)
-a=0
-stringx= []
-stringy = []
-#位置
-def search(key):
+def search(key):#位置
     for char, row in enumerate(keyboard):
         if key in row:
             stringx.append(char)
             stringy.append(row.index(key))
+#主函数
+string = input("Enter a string to type: ")
+moves = []
+if not string.isalpha():
+    print(f"The string cannot be typed out.")
+    exit(0)
 
+stringx = []
+stringy = []
 
-
-
+for char in string:
+    search(char)
 
 x,y = 0,0
-print(f"The robot must perform the following operations:", end = "")
 
-for s in range(len(string)):
-    move(x,y,stringx[s],stringy[s])
+for s in range(len(stringx)):
+    x,y = move(x,y,stringx[s],stringy[s])
 
 
+print(f"The robot must perform the following operations:\n{''.join(moves)}", end = "")
 
