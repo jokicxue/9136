33656bdec1cf93502fcc2264f4455808d7b19c15
Tiancheng XUE <jokicxue@gmail.com>
2024-08-16 09:58:29 +1000

    week4 task9 pass

--- assignments1/week4/task9_pass_origin.py
+++ assignments1/week4/task9_pass_origin.py
@@ -42,9 +42,24 @@ def move(x0, y0, x1, y1, i):
                 moves[i].append('r' * y_distance)
         else:  # Wrapping
             if y1 < y0:
-                moves[i].append('r' * y_distance + 'w')
+                for dis in range(y_sround):
+                    moves[i].append('r')
+                    y0 += 1
+                    if y0 == num_col:
+                        y0 = 0
+                        moves[i].append('w')
+
+                    #moves[i].append('r' * y_distance + 'w')
             else:
-                moves[i].append('l' * y_distance + 'w')
+                for dis in range(y_sround):
+                    moves[i].append('l')
+                    y0 -= 1
+                    if y0 == -1:
+                        y0 = num_col-1
+                        moves[i].append('w')
+
+
+                #moves[i].append('l' * y_distance + 'w')
 
     if x0 != x1:
         if x_distance == abs(x1 - x0) and x_distance != x_sround:  # No wrapping
@@ -54,12 +69,24 @@ def move(x0, y0, x1, y1, i):
                 moves[i].append('d' * x_distance)
         else:  # Wrapping
             if x1 < x0:
-                moves[i].append('d' * x_distance + 'w')
+                for dis in range(x_sround):
+                    moves[i].append('d')
+                    x0 += 1
+                    if x0 == num_row:
+                        x0 = 0
+                        moves[i].append('w')
+                #moves[i].append('d' * x_distance + 'w')
             else:
-                moves[i].append('u' * x_distance + 'w')
+                for dis in range(x_sround):
+                    moves[i].append('u')
+                    x0 -= 1
+                    if x0 == -1:
+                        x0 = num_row-1
+                        moves[i].append('w')
+                #moves[i].append('u' * x_distance + 'w')
 
     moves[i].append('p')
-    print(moves)
+    #print(moves)
     return x1, y1
 
 
@@ -80,16 +107,25 @@ def verify(string, keyboards):
 
 def shortest(moves):
     moves0 = [[],[],[],[]]
+    filtered_list = [[],[],[],[]]
+    moves1 = [[],[],[],[]]
     for row in range(len(moves)):
         moves0[row] = ''.join(moves[row])
-
-    shortest = max(moves0, key=len)
-    index = moves0.index(shortest)
-    for i in range(len(moves0)):
-        if len(moves0[i]) < len(shortest) and len(moves0[i]) != 0:
-            shortest = moves0[i]
+    #print(moves0)
+    for x in range(len(moves0)):
+        filtered = list(filter(lambda x: x != 'w', moves0[x]))
+        moves1[x] = filtered
+    #print(moves1)
+    for row in range(len(moves1)):
+        filtered_list[row] = ''.join(moves1[row])
+    #print(filtered_list)
+    shortest = max(filtered_list, key=len)
+    index = filtered_list.index(shortest)
+    for i in range(len(filtered_list)):
+        if len(filtered_list[i]) < len(shortest) and len(filtered_list[i]) != 0:
+            shortest = filtered_list[i]
             index = i
-    return shortest, index
+    return shortest, index,moves0[index]
 #主函数
 string = input("Enter a string to type: ")
 
@@ -113,7 +149,7 @@ for k in range(len(num)):
         x,y = move(x,y,stringx[num[k]][s],stringy[num[k]][s],num[k])
     x,y = 0,0
 
-shortest0, index_short = shortest(moves)
+shortest0, index_short, movelist = shortest(moves)
 #print(moves)
 #print(shortest0,index_short)
 #print(num, stringx, stringy, moves, sep="\n")
@@ -125,5 +161,6 @@ for row in kb[index_short]:
     print(f"| {row} |")
 print('-'*(len(kb[index_short][0])+4))
 
-print(f"The robot must perform the following operations:\n{''.join(shortest0)}", sep="\n")
-#print(num, stringx, stringy, moves, sep="\n")
+print(f"The robot must perform the following operations:\n{''.join(movelist)}", sep="\n")
+#print(num, stringx, stringy, sep="\n")
+#print(f"{''.join(moves[0])},{''.join(moves[1])},{''.join(moves[2])},{''.join(moves[3])}")
