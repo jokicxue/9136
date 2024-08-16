c58d2a9fbafca3261ddd091073c0cc6cb239b01c
Tiancheng XUE <jokicxue@gmail.com>
2024-08-16 10:27:30 +1000

    week4 task9 pass

--- assignments1/week4/task9.py
+++ assignments1/week4/task9.py
@@ -1,73 +1,68 @@
-kb = [ ["abcdefghijklm",
-       "nopqrstuvwxyz"],
-
-    ["789",
-        "456",
-        "123",
-        "0.-"],
-
-    ["chunk",
-        "vibex",
-        "gymps",
-        "fjord",
-        "waltz"],
-
-    ["bemix",
-        "vozhd",
-        "grypt",
-        "clunk",
-        "waqfs"]]
+kb = [ ["abcdefghijklm", "nopqrstuvwxyz"],
+     ["789", "456", "123",  "0.-"],
+    ["chunk", "vibex",  "gymps", "fjord", "waltz"],
+    ["bemix", "vozhd","grypt","clunk", "waqfs"]]
 
 def sround_distance(pos1, pos2, length):
     direct = abs(pos1 - pos2)
     sround = length - direct
-    return min(sround, direct), sround
+    return min(sround, direct),sround
 
 def move(x0, y0, x1, y1, i):
     keyboard = kb[i]
     num_row = len(keyboard)
     num_col = len(keyboard[0])
 
-    while (x0, y0) != (x1, y1):
-        # Handle horizontal movement (y)
-        if y0 != y1:
-            y_distance, y_sround = sround_distance(y0, y1, num_col)
-            if y_distance == abs(y1 - y0) and y_distance != y_sround:  # No wrapping
-                if y1 < y0:
-                    y0 -= 1
-                    moves[i].append('l')
-                else:
-                    y0 += 1
+    y_distance, y_sround = sround_distance(y0, y1, num_col)
+    x_distance, x_sround = sround_distance(x0, x1, num_row)
+
+    if y0 != y1:
+        if y_distance == abs(y1 - y0) and y_distance != y_sround:  # No wrapping
+            if y1 < y0:
+                moves[i].append('l' * y_distance)
+            else:
+                moves[i].append('r' * y_distance)
+        else:  # Wrapping
+            if y1 < y0:
+                for dis in range(y_sround):
                     moves[i].append('r')
-            else:  # Wrapping
-                if y1 < y0:
-                    y0 = (y0 + 1) % num_col  # Wrap around to the right
-                    moves[i].append('rw')
-                else:
-                    y0 = (y0 - 1) % num_col  # Wrap around to the left
-                    moves[i].append('lw')
+                    y0 += 1
+                    if y0 == num_col:
+                        y0 = 0
+                        moves[i].append('w')
 
-        # Handle vertical movement (x)
-        if x0 != x1:
-            x_distance,x_sround = sround_distance(x0, x1, num_row)
-            if x_distance == abs(x1 - x0) and x_distance != x_sround:  # No wrapping
-                if x1 < x0:
-                    x0 -= 1
-                    moves[i].append('u')
-                else:
-                    x0 += 1
+            else:
+                for dis in range(y_sround):
+                    moves[i].append('l')
+                    y0 -= 1
+                    if y0 == -1:
+                        y0 = num_col-1
+                        moves[i].append('w')
+
+    if x0 != x1:
+        if x_distance == abs(x1 - x0) and x_distance != x_sround:  # No wrapping
+            if x1 < x0:
+                moves[i].append('u' * x_distance)
+            else:
+                moves[i].append('d' * x_distance)
+        else:  # Wrapping
+            if x1 < x0:
+                for dis in range(x_sround):
                     moves[i].append('d')
-            else:  # Wrapping
-                if x1 < x0:
-                    x0 = (x0 + 1) % num_row  # Wrap around downward
-                    moves[i].append('dw')
-                else:
-                    x0 = (x0 - 1) % num_row  # Wrap around upward
-                    moves[i].append('uw')
-
-    # Finally, add the 'p' action for pressing the key
+                    x0 += 1
+                    if x0 == num_row:
+                        x0 = 0
+                        moves[i].append('w')
+            else:
+                for dis in range(x_sround):
+                    moves[i].append('u')
+                    x0 -= 1
+                    if x0 == -1:
+                        x0 = num_row-1
+                        moves[i].append('w')
     moves[i].append('p')
     return x1, y1
+
 
 def search(key,keyboard,i):#位置
 
@@ -86,16 +81,25 @@ def verify(string, keyboards):
 
 def shortest(moves):
     moves0 = [[],[],[],[]]
+    filtered_list = [[],[],[],[]]
+    moves1 = [[],[],[],[]]
     for row in range(len(moves)):
         moves0[row] = ''.join(moves[row])
 
-    shortest = max(moves0, key=len)
-    index = moves0.index(shortest)
-    for i in range(len(moves0)):
-        if len(moves0[i]) < len(shortest) and len(moves0[i]) != 0:
-            shortest = moves0[i]
+    for x in range(len(moves0)):
+        filtered = list(filter(lambda x: x != 'w', moves0[x]))
+        moves1[x] = filtered
+
+    for row in range(len(moves1)):
+        filtered_list[row] = ''.join(moves1[row])
+
+    shortest = max(filtered_list, key=len)
+    index = filtered_list.index(shortest)
+    for i in range(len(filtered_list)):
+        if len(filtered_list[i]) < len(shortest) and len(filtered_list[i]) != 0:
+            shortest = filtered_list[i]
             index = i
-    return shortest, index
+    return shortest, index,moves0[index]
 #主函数
 string = input("Enter a string to type: ")
 
@@ -119,11 +123,7 @@ for k in range(len(num)):
         x,y = move(x,y,stringx[num[k]][s],stringy[num[k]][s],num[k])
     x,y = 0,0
 
-shortest0, index_short = shortest(moves)
-#print(moves)
-#print(shortest0,index_short)
-#print(num, stringx, stringy, moves, sep="\n")
-
+shortest0, index_short, movelist = shortest(moves)
 
 print("Configuration used:")
 print('-'*(len(kb[index_short][0])+4))
@@ -131,6 +131,4 @@ for row in kb[index_short]:
     print(f"| {row} |")
 print('-'*(len(kb[index_short][0])+4))
 
-print(f"The robot must perform the following operations:\n{''.join(shortest0)}", sep="\n")
-print(num, stringx, stringy, ''.join(moves[0]),''.join(moves[1]),''.join(moves[2]),''.join(moves[3]), sep="\n")
-
+print(f"The robot must perform the following operations:\n{''.join(movelist)}", sep="\n")
